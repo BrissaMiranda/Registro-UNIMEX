d35d063e4364a73604fa0099e2923360e06984ec
Pamela936 <pam1520chavez0304@gmail.com>
2026-07-21 19:36:44 -0600

    Create EstudiantePerfilViewModel.cs

---
+++ ViewModels/EstudiantePerfilViewModel.cs
@@ -0,0 +1,25 @@
+namespace SistemaUniversidad.ViewModels
+{
+    public class EstudiantePerfilViewModel
+    {
+        public string Usuario { get; set; } = "";
+        
+        public string NombreCompleto { get; set; } = "";
+
+        public string Carnet { get; set; } = "";
+
+        public string Carrera { get; set; } = "";
+
+        public string Facultad { get; set; } = "";
+
+        public string Correo { get; set; } = "";
+
+        public string Telefono { get; set; } = "";
+
+        public int Edad { get; set; }
+
+        public int VecesInicioSesion { get; set; }
+
+        public DateTime? FechaUltimoAcceso { get; set; }
+    }
+}
