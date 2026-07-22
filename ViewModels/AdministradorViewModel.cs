a85b38c9b769e8636b12a26dcf04d70368b78c7a
Jos726 <josuerama8@gmail.com>
2026-07-21 19:06:29 -0600

    Create AdministradorViewModel.cs

---
+++ ViewModels/AdministradorViewModel.cs
@@ -0,0 +1,17 @@
+namespace SistemaUniversidad.ViewModels
+{
+    public class AdministradorViewModel
+    {
+        public List<UsuarioViewModel> Usuarios { get; set; } = new();
+
+        public int TotalUsuarios { get; set; }
+
+        public int TotalEstudiantes { get; set; }
+
+        public int TotalDocentes { get; set; }
+
+        public int TotalAdministradores { get; set; }
+
+        public int UsuariosActivos { get; set; }
+    }
+}
