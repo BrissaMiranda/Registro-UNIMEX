314e8bda3665a59b2a7d11b0cdae9c2bb230590b
Juanitos1 <juaneduardolj@hotmail.com>
2026-07-21 19:22:20 -0600

    Create Docente.cs

---
+++ Models/Docente.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaUniversidad.Models
+{
+    public class Docente
+    {
+        [Key]
+        public int IdDocente { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Apellido { get; set; } = string.Empty;
+
+        public string Facultad { get; set; } = string.Empty;
+
+        public string Especialidad { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
+
+        public string Telefono { get; set; } = string.Empty;
+
+        public int? IdUsuario { get; set; }
+
+        public Usuario? Usuario { get; set; }
+    }
+}
