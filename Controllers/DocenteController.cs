1728a4179d61f2684cc060c7de33a0cea66339df
Juanitos1 <juaneduardolj@hotmail.com>
2026-07-21 19:22:54 -0600

    Create DocenteController.cs

---
+++ Controllers/DocenteController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using SistemaUniversidad.Base;
+using SistemaUniversidad.Services;
+
+namespace SistemaUniversidad.Controllers
+{
+    public class DocenteController : BaseController
+    {
+        private readonly PerfilService _perfilService;
+
+        public DocenteController(PerfilService perfilService)
+        {
+            _perfilService = perfilService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!SesionIniciada())
+                return RedirigirLogin();
+
+            if (RolUsuario() != "Docente")
+                return RedirigirLogin();
+
+            int idUsuario = HttpContext.Session.GetInt32("IdUsuario")!.Value;
+
+            var perfil = await _perfilService.ObtenerPerfilDocente(idUsuario);
+
+            if (perfil == null)
+                return RedirigirLogin();
+
+            return View(perfil);
+        }
+    }
+}
